fe5dedda58f46c55f9424613b02c579c7fdb488a
DelroyGayle <delroygayle01@gmail.com>
2024-02-02 15:26:30 +0000

    Customise Admin for Posters

--- posters/admin.py
+++ posters/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Poster, Category
 
+class PosterAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 # Register your models here.
-admin.site.register(Poster)
-admin.site.register(Category)
+admin.site.register(Poster, PosterAdmin)
+admin.site.register(Category, CategoryAdmin)
