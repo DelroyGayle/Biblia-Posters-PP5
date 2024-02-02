c0ca5c5f5d922d83e5718238f5b4467335f18d88
DelroyGayle <delroygayle01@gmail.com>
2024-02-02 15:39:32 +0000

    Add posters' views and templates

--- posters/views.py
+++ posters/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Poster
+
+def all_posters(request):
+    """ A view to show all posters, including sorting and search queries """
+
+    posters = Poster.objects.all()
+
+    context = {
+        'posters': posters,
+    }
+
+    return render(request, 'posters/posters.html', context)
