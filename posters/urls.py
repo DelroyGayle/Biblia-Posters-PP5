386054631183a2989ee672480759f0af4e416cb3
DelroyGayle <delroygayle01@gmail.com>
2024-02-03 17:39:03 +0000

    Add search functionality

--- posters/urls.py
+++ posters/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_posters, name='posters'),
+    path('', views.fetch_posters, name='posters'),
     path('<poster_id>', views.poster_details, name='poster_details'),
 ]
--- posters/views.py
+++ posters/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 
 from .models import Poster
 
-def all_posters(request):
+def fetch_posters(request):
     """ A view to show all posters, including sorting and search queries """
 
     posters = Poster.objects.all()
+    query = None
+
+    if request.GET:
+        if 'query' in request.GET:
+            query = request.GET['query']
+            if not query:
+                messages.error(request, "You did not enter any search criteria!")
+                return redirect(reverse('posters'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            posters = posters.filter(queries)
+
+            # Check for no matches
+            if not posters:
+                messages.error(request, "There are no posters that match the search criteria!")
+                return redirect(reverse('posters'))
 
     context = {
         'posters': posters,
+        'search_term': query,
     }
 
     return render(request, 'posters/posters.html', context)
