8ce5093e4c0a90d9569c860802428c6a9fa7af91
DelroyGayle <delroygayle01@gmail.com>
2024-02-04 01:21:15 +0000

    Add sorting

--- posters/views.py
+++ posters/views.py
@@ -1,40 +1,78 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 from .models import Poster, Category
 
 def fetch_posters(request):
-    """ A view to show all posters, including sorting and search queries """
+    """ A view to show all posters, including categorising, sorting and search queries """
 
+    # Retrieve all the available posters
     posters = Poster.objects.all()
+
+    # Reset all parameters to None
     query = None
     categories = None
+    the_sort_type = None
+    the_sort_direction = None
 
     if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            posters = posters.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+        # Nonnull request.GET
+        if 'sort' in request.GET:
+            # 1) Determine the type of sort
+            the_sort_key = request.GET['sort']
+            the_sort_type = the_sort_key
+            if the_sort_key == 'name':
+                """ 
+                For case insensitive 'name' searches
+                Use 'lower_name' as the value of 'the_sort_key'
+                Create an annotate field called 'lower_name' which holds
+                the value of the name field in lowercase
+                """
+                the_sort_key = 'lower_name'
+                posters = posters.annotate(lower_name=Lower('name'))
+                print(posters) # TODO
 
-        if 'query' in request.GET:
-            query = request.GET['query']
-            if not query:
-                messages.error(request, "You did not enter any search criteria!")
-                return redirect(reverse('posters'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            posters = posters.filter(queries)
+            # 2) Determine the direction of the sort whether ascending or descending
+            if 'direction' in request.GET:
+                the_sort_direction = request.GET['direction']
+                if the_sort_direction == 'desc':
+                    # Make the sort direction: Descending
+                    the_sort_key = f'-{the_sort_key}'
 
-            # Check for no matches
-            if not posters:
-                messages.error(request, "There are no posters that match the search criteria!")
-                return redirect(reverse('posters'))
+            # 3) Perform the Sort
+            posters = posters.order_by(the_sort_key)
+
+            # 4) Filter in regard to the selected category
+            if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                posters = posters.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
+
+            # 5) Filter in regard to the selected search query
+            if 'query' in request.GET:
+                query = request.GET['query']
+                if not query:
+                    # Null entry
+                    messages.error(request, "You did not enter any search criteria!")
+                    return redirect(reverse('posters'))
+                
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                posters = posters.filter(queries)
+
+                # Check for no match
+                if not posters:
+                    messages.error(request, "There are no posters that match the search criteria!")
+                    return redirect(reverse('posters'))
+
+    current_sorting = f'{the_sort_type}_{the_sort_direction}'
 
     context = {
         'posters': posters,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'posters/posters.html', context)
