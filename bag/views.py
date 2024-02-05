05547d3a199563cd583c54aa8e8efc5a3cd8af88
DelroyGayle <delroygayle01@gmail.com>
2024-02-05 20:04:29 +0000

    Add adjust and remove functionality to shopping bag page

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -10,7 +10,7 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of the specified item to the shopping bag """
 
     # Convert string value into an integer
     quantity = int(request.POST.get('quantity'))
@@ -23,10 +23,47 @@ def add_to_bag(request, item_id):
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
-    # Add item and its quantity to the bag
+        # Add item and its quantity to the bag
         bag[item_id] = quantity
 
-    # Store in session variable
+    # Update session variable
     request.session['bag'] = bag
-    
+
     return redirect(currentpage_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified item to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    # Fetch the current contents of this session's shopping bag
+    # Default value is {} if None
+    bag = request.session.get('bag', {})
+    # Update new quantity
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        # Zero entered therefore remove the item entirely
+        bag.pop(item_id)
+
+    # Update session variable
+    request.session['bag'] = bag
+    # Reload page with the Shopping Bag contents
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        # Fetch the current contents of this session's shopping bag
+        # Default value is {} if None
+        bag = request.session.get('bag', {})
+        # remove the item entirely
+        bag.pop(item_id)
+        # Update session variable
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
