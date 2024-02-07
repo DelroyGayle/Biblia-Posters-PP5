727e5ce0c75d15b68ff375a6847f0ece320e68f5
DelroyGayle <delroygayle01@gmail.com>
2024-02-07 16:53:24 +0000

    Test checkout/views.py and endpoint /checkout

--- checkout/views.py
+++ checkout/views.py
@@ -7,13 +7,14 @@ from .forms import OrderForm
 
 import stripe
 
+
 def checkout(request):
     stripePublicKey = settings.STRIPE_PUBLIC_KEY
-    stripeSecretKey = settings.STRIPE_SECRET_KEY 
+    stripeSecretKey = settings.STRIPE_SECRET_KEY
 
     bag = request.session.get('bag', {})
     if not bag:
-        messages.error(request, 
+        messages.error(request,
                        "There's nothing in your bag at the moment")
         # redirect back to the posters page
         return redirect(reverse('posters'))
@@ -28,11 +29,16 @@ def checkout(request):
     )
 
     order_form = OrderForm()
+
+    if not stripePublicKey:
+        messages.warning(request, ("Stripe public key is missing. "
+            "Did you forget to set it in your environment?"))
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripePublicKey,
-        'client_secret': intent.client_secret,        
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
