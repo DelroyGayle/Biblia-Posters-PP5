5fa0fc319e421545cfcb6c1bface5a387ed98b95
DelroyGayle <delroygayle01@gmail.com>
2024-02-07 16:41:02 +0000

    Test PaymentIntent

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,9 @@ from .forms import OrderForm
 import stripe
 
 def checkout(request):
+    stripePublicKey = settings.STRIPE_PUBLIC_KEY
+    stripeSecretKey = settings.STRIPE_SECRET_KEY 
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 
@@ -18,13 +21,18 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripeSecretKey
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OUTZrHPvBtuEw4nlFFEsWmygw30ndXtlT9zC9tOHGuJCpOVoYhBjxXq2kA2GSV9oh53JPjrTVsduoGJGEkXzgDx00edeu7wSX',
-        'client_secret': 'test client secret',        
+        'stripe_public_key': stripePublicKey,
+        'client_secret': intent.client_secret,        
     }
 
     return render(request, template, context)
