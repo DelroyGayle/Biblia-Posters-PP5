1589ab9b3618b5ea8518152bba4f17fb969768ef
DelroyGayle <delroygayle01@gmail.com>
2024-02-09 15:59:43 +0000

    Profiles - Add order history and update checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
@@ -19,7 +15,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'grand_total', 'original_bag',
                        'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,8 @@ from bag.context_processors import bag_contents
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from posters.models import Poster
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -117,7 +119,41 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        # Attempt to prefill the form with any info
+        # the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+
+                # Populate the Full Name from the User model
+                # if the Profile Full Name is null
+                the_fullname = profile.user.get_full_name()
+                if not the_fullname:
+                    the_fullname = request.user.get_username()
+
+                # Populate the Email Address from the User model
+                # if the Profile Email Address is null
+                the_email = profile.user.email
+                if not the_email:
+                    the_email = request.user.email
+
+                order_form = OrderForm(initial={
+                    'full_name': the_fullname,
+                    'email': the_email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'city': profile.default_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+
+        else:
+            order_form = OrderForm()
 
     if not stripePublicKey:
         messages.warning(request, ("Stripe public key is missing. "
@@ -140,6 +176,30 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    # At this point the order was successful
+    # Therefore if the user is logged in
+    # Attach the user's profile to the order at this point
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_city': order.city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, ('Order successfully processed! '
                                f'Your order number is {order_number}. '
                                'A confirmation email will be sent to '
