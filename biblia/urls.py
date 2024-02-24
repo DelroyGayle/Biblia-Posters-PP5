797c7784430adf94c56dacc0d39f7e171d9d2c11
DelroyGayle <delroygayle01@gmail.com>
2024-02-24 16:31:56 +0000

    Add 500 page

--- biblia/urls.py
+++ biblia/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
+from .views import handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +32,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'boutique_ado.views.handler404'
+handler500 = 'boutique_ado.views.handler500'
