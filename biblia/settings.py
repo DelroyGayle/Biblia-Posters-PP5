8429f982e8d04e5d809353c13ca483aeca4ca492
DelroyGayle <delroygayle01@gmail.com>
2024-02-25 13:26:57 +0000

    Revert to local; redo all migrations

--- biblia/settings.py
+++ biblia/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY','')              
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # 'DEVELOPMENT' in os.environ
+DEBUG = True # False # 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['biblia-posters-dg-869e3a15ddae.herokuapp.com', 'localhost',
                  '8000-delroygayle-bibliaposte-dlbeacwiw6l.ws-eu108.gitpod.io']
@@ -229,7 +229,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-if 'DEVELOPMENT' in os.environ:
+if 'DEVELOPMENT' in os.environ or True:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'biblia@example.com'
 else:
