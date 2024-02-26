bc1e57f52828de598f4c0d6f1016584e7a197044
DelroyGayle <delroygayle01@gmail.com>
2024-02-26 18:36:26 +0000

    Add robots.txt, sitemap.xml and 500.html

--- biblia/urls.py
+++ biblia/urls.py
@@ -32,4 +32,6 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'biblia.views.handler404'
+handler500 = 'biblia.views.handler500'
+
 
--- biblia/views.py
+++ biblia/views.py
@@ -4,6 +4,8 @@ def handler404(request, exception):
 	""" Error Handler 404 - Page Not Found """ 
 	return render(request, "errors/404.html", status=404)
 
-def handler500(request, exception):
-	""" Error Handler 500 - Internal Server Error """ 
-	return render(request, "errors/500.html", status=500)
+
+def handler500(request, *args, **argv):
+	""" Error Handler 500 - Internal Server Error """
+	return render(request, '500.html', status=500)
+
