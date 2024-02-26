32a352b709eef9b2298a8ef5c657bde4367289cd
DelroyGayle <delroygayle01@gmail.com>
2024-02-26 14:50:07 +0000

    Test Real Emails

--- biblia/settings.py
+++ biblia/settings.py
@@ -27,13 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY','')              
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['biblia-posters-dg-869e3a15ddae.herokuapp.com',
-                 'localhost', '127.0.0.1']
+        'localhost', '127.0.0.1']
 
 # Application definition
 
